6e34872ebc18e43459e8923a4721a916b17cc4fe
Oni Bashir Muh'Bashir <108095967+mbob6@users.noreply.github.com>
2024-11-10 11:50:48 +0100

    added StringReverse

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CodeChallenge
+{
+    class Challenge
+    {
+        public static void Main(string[] args)
+        {
+            ReverseString();
+            
+            
+            Console.ReadLine();
+        }
+        
+        static void ReverseString()
+        {
+            Console.WriteLine("Enter a word to reverse: ");
+            string? word = Console.ReadLine();
+
+            Console.WriteLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.Write(word[i]);
+            }
+
+            Console.WriteLine("\n");
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                Console.Write(word[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
